a515a5e6fa97e238683536643d94c310079bd55c
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-09-25 14:15:10 -0300

    fix absolute imports paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from "next/app";
-import { Header } from "../components";
+
+import { Header } from "@/components/Header";
 
 import "@styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <div>
-    <Header />
-    <Component {...pageProps} />
-  </div>
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
   );
 }
