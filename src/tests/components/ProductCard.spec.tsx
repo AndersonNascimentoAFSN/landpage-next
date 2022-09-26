3242b9b00fe87d3a6e2eb1ec0fa5d6e6fe6d71cd
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-09-26 16:58:23 -0300

    add more test ProductCard

--- src/tests/components/ProductCard.spec.tsx
+++ src/tests/components/ProductCard.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { ProductCard } from "@/components/ProductCard";
 
@@ -32,5 +33,42 @@ describe("Product component", () => {
       name: /Image product/i,
     });
     expect(imageProduct).toBeInTheDocument();
+  });
+
+  it("should mark the product as a favorite", async () => {
+    const { getByRole } = render(
+      <ProductCard
+        id="1"
+        name="Camiseta Evangélica"
+        imageURL="./public/logo.svg"
+        listPrice="R$ 50,00"
+        salePrice="R$ 55,00"
+        isFavorite={true}
+        onClick={() => {}}
+      />
+    );
+
+    const favoriteButton = getByRole("button");
+    expect(favoriteButton).toHaveAttribute("data-testId", "marked-favorite");
+  });
+
+  it("should not mark the product as a favorite", async () => {
+    const { getByRole } = render(
+      <ProductCard
+        id="1"
+        name="Camiseta Evangélica"
+        imageURL="./public/logo.svg"
+        listPrice="R$ 50,00"
+        salePrice="R$ 55,00"
+        isFavorite={false}
+        onClick={() => {}}
+      />
+    );
+
+    const favoriteButton = getByRole("button");
+    expect(favoriteButton).not.toHaveAttribute(
+      "data-testId",
+      "marked-favorite"
+    );
   });
 });
